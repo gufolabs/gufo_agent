4d34a54e2c7f75767f5407fd89b106170273b872
Dmitry Volodin <dv@nocproject.org>
2023-04-28 10:05:58 +0200

    Fix format

--- collectors/block_io/src/lib.rs
+++ collectors/block_io/src/lib.rs
@@ -18,15 +18,31 @@ pub struct Collector;
 
 // Generated metrics
 counter!(read_ios, "Number of read I/Os processed", dev);
-counter!(read_merges, "Number of read I/Os merged with in-queue I/O", dev);
+counter!(
+    read_merges,
+    "Number of read I/Os merged with in-queue I/O",
+    dev
+);
 counter!(read_sectors, "Number of sectors read", dev);
 counter!(read_ticks, "Total wait time for read requests, ms", dev);
 counter!(write_ios, "Number of write I/Os processed", dev);
-counter!(write_merges, "Number of write I/Os merged with in-queue I/O", dev);
+counter!(
+    write_merges,
+    "Number of write I/Os merged with in-queue I/O",
+    dev
+);
 counter!(write_sectors, "Number of sectors written", dev);
 counter!(write_ticks, "Total wait time for write requests, ms", dev);
-gauge!(in_flight, "Number of I/Os currently in flight, requests", dev);
-gauge!(io_ticks, "Total time this block device has been active, ms", dev);
+gauge!(
+    in_flight,
+    "Number of I/Os currently in flight, requests",
+    dev
+);
+gauge!(
+    io_ticks,
+    "Total time this block device has been active, ms",
+    dev
+);
 gauge!(time_in_queue, "Total wait time for all requests, ms", dev);
 
 // Instantiate collector from given config
