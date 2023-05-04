51838c178aeeeeef33665a6de200d62d8a0af8f9
Dmitry Volodin <dv@nocproject.org>
2023-05-04 13:32:18 +0200

    build-all: Set rust arch

--- tools/build/build-all.sh
+++ tools/build/build-all.sh
@@ -6,11 +6,13 @@ if [ -z "$VERSION" ]; then
     echo "VERSION is not set"
     exit 1
 fi
+echo "Building gufo-agent ${VERSION}"
 # Create dist
 [ -d dist/ ] || mkdir dist
 # Define build function
 build_target_tgz()
 {
+    rustup target add $1
     cargo build --release --target $1
     (
         cd target/$1/release \
