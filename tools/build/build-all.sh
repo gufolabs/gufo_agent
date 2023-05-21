fee2c5873d5dd2cf021e41bd6f5a5ec758b0a45a
Dmitry Volodin <dv@nocproject.org>
2023-05-21 14:39:56 +0200

    Native .deb packaging

--- tools/build/build-all.sh
+++ tools/build/build-all.sh
@@ -25,19 +25,37 @@ build_target_tgz()
 }
 # Prepare structure for debian package
 # $1 - arch
-prepare_deb()
+build_deb()
 {
-    root="dist/deb/$1"
+    arch=$1
+    root="dist/deb/$arch"
     # Cleanup and recreate directory
     [ -d $root ] && rm -r $root
     mkdir -p $root
     # Inner directory structure
+    mkdir -p "$root/DEBIAN"
     mkdir -p "$root/usr/bin"
     mkdir -p "$root/usr/share/man/man1"
     # Copy binary
-    tar -x -z -f ./dist/gufo-agent-${VERSION}_linux_$1.tgz -C $root/usr/bin
+    tar -x -z -f ./dist/gufo-agent-${VERSION}_linux_$arch.tgz -C $root/usr/bin
     # Copy man
     cp man/gufo-agent.1 $root/usr/share/man/man1
+    # Get package size
+    PACKAGE_ROOT_SIZE_BYTES=$(du -bcs --exclude=DEBIAN $root | head -1 | awk '{print $1}' | sed -e s/^0\+//)
+    # To kB
+    INPUT_INSTALLED_SIZE="$(awk -v size="$PACKAGE_ROOT_SIZE_BYTES" 'BEGIN {print (size/1024)+1}' | awk '{print int($0)}')"
+    # Write DEBIAN/control
+    cat > $root/DEBIAN/control << __EOF__
+Package: gufo-agent
+Version: ${VERSION}
+Installed-Size: ${INPUT_INSTALLED_SIZE}
+Architecture: ${arch}
+Maintainer: Gufo Labs
+
+an universal agent for infrastructure monitoring
+__EOF__
+    # Build deb
+    dpkg-deb -Zgzip -b $root "dist/gufo-agent_${VERSION}_${arch}.deb"
 }
 # Prepare structure for rpm package
 # $1 - arch
@@ -57,9 +75,9 @@ prepare_rpm()
 }
 # Build x86_64-unknown-linux-gnu + deb
 build_target_tgz x86_64-unknown-linux-gnu linux_amd64
-prepare_deb amd64
+build_deb amd64
 prepare_rpm amd64
 # Build aarch64-unknown-linux-gnu + deb
 build_target_tgz aarch64-unknown-linux-gnu linux_aarch64
-prepare_deb aarch64
+build_deb aarch64
 prepare_rpm aarch64
